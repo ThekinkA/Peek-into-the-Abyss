36523c1043dad3352d8a5144537aabdeba39c0f8
Elloper <1908461929@qq.com>
2025-04-16 22:08:58 +0800

    feat: 更新统计页面，添加数据展示卡片和样式

--- src/pages/DataCollection/Statistics.tsx
+++ src/pages/DataCollection/Statistics.tsx
@@ -1,10 +1,86 @@
+import {
+  AimOutlined,
+  CalendarOutlined,
+  GlobalOutlined,
+  NodeIndexOutlined,
+} from '@ant-design/icons'; // 导入图标
+import { Col, Row } from 'antd';
 import React from 'react';
+import './Statistics.css'; // 导入CSS文件
 
 const Statistics: React.FC = () => {
   return (
     <div>
-      <h1>采集数据统计</h1>
-      <p>这是采集数据统计页面的内容。</p>
+      <Row gutter={16}>
+        <Col span={6}>
+          <div className="card">
+            <div style={{ padding: '20px', position: 'relative' }}>
+              <div className="subtitle">已检测天数</div>
+              <div className="text">16</div>
+              <CalendarOutlined
+                style={{
+                  fontSize: '40px',
+                  color: '#1890ff',
+                  position: 'absolute',
+                  bottom: '20px',
+                  right: '20px',
+                }}
+              />
+            </div>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="card">
+            <div style={{ padding: '20px', position: 'relative' }}>
+              <div className="subtitle">已检测节点总数</div>
+              <div className="text">6,560</div>
+              <NodeIndexOutlined
+                style={{
+                  fontSize: '40px',
+                  color: '#52c41a',
+                  position: 'absolute',
+                  bottom: '20px',
+                  right: '20px',
+                }}
+              />
+            </div>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="card">
+            <div style={{ padding: '20px', position: 'relative' }}>
+              <div className="subtitle">已检测C段IP总数</div>
+              <div className="text">126,560</div>
+              <GlobalOutlined
+                style={{
+                  fontSize: '40px',
+                  color: '#faad14',
+                  position: 'absolute',
+                  bottom: '20px',
+                  right: '20px',
+                }}
+              />
+            </div>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="card">
+            <div style={{ padding: '20px', position: 'relative' }}>
+              <div className="subtitle">生成攻击路径总数</div>
+              <div className="text">78</div>
+              <AimOutlined
+                style={{
+                  fontSize: '40px',
+                  color: '#f5222d',
+                  position: 'absolute',
+                  bottom: '20px',
+                  right: '20px',
+                }}
+              />
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
