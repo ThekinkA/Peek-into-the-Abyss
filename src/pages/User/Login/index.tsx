4155e1e2f5874199ca725d045c255745f45187a0
viod_xiao <viod_xiao@outlook.com>
2025-06-16 12:47:29 +0800

    修改了登录界面的标题等

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -170,7 +170,7 @@ const Login: React.FC = () => {
             maxWidth: '75vw',
           }}
           logo={<img alt="logo" src="/logo.png" />}
-          title="暗网态势感知平台"
+          title="暗网态势感知平台 "
           subTitle={intl.formatMessage({ id: 'pages.layouts.userLayout.title' })}
           initialValues={{
             autoLogin: true,
