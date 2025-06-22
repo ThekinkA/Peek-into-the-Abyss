85f0ed0057fd7eba07a830c9e9007eab419b7a21
Elloper <1908461929@qq.com>
2025-06-22 19:36:37 +0800

    Refactor login error messages and placeholders for improved clarity
    - Updated error message for incorrect username and password in Chinese localization.
    - Simplified placeholder text for username and password fields in the login form.
    - Adjusted related messages in the login component to reflect the changes in localization.

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -213,7 +213,7 @@ const Login: React.FC = () => {
             <LoginMessage
               content={intl.formatMessage({
                 id: 'pages.login.accountLogin.errorMessage',
-                defaultMessage: '账户或密码错误(admin/ant.design)',
+                defaultMessage: '账户或密码错误',
               })}
             />
           )}
@@ -227,7 +227,7 @@ const Login: React.FC = () => {
                 }}
                 placeholder={intl.formatMessage({
                   id: 'pages.login.username.placeholder',
-                  defaultMessage: '用户名: admin or user',
+                  defaultMessage: '用户名:',
                 })}
                 rules={[
                   {
@@ -249,7 +249,7 @@ const Login: React.FC = () => {
                 }}
                 placeholder={intl.formatMessage({
                   id: 'pages.login.password.placeholder',
-                  defaultMessage: '密码: ant.design',
+                  defaultMessage: '密码:',
                 })}
                 rules={[
                   {
